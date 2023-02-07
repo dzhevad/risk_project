4eba411a760fa8d414e0afc3258df5a6127c1a04
Seyed Mohammad Javad Mousavi <120303241+dzhevad@users.noreply.github.com>
2023-02-07 20:00:39 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,13 @@
+// library:     C:\Program Files (x86)\Embarcadero\Dev-Cpp\allegro\lib
+// include:     C:\Program Files (x86)\Embarcadero\Dev-Cpp\allegro\include
 
 
 
 #include <stdio.h>
 #include <stdlib.h>
+#include<time.h>
+#include<conio.h>
+
 #include<allegro5/allegro.h>
 #include<allegro5/allegro_font.h>
 #include<allegro5/allegro_ttf.h>
@@ -15,7 +20,57 @@
 
 void draw_map();
 
+void print();
+
+void esmgozari(); 
+
+
+
+int daryaft_sarbaz(int tedad_zamin , char bazikon ); 
+
+void pakhsh_khodkar_sarbaz();
+
+
+
+
+
+
+
+int tas();
+
+void sort(int array[] , int size);
+
+
+struct  player1{
+	int sarbaz ;
+	int tedad_zamin;
+}player1;
+
+struct  player2{
+	int sarbaz;
+	int tedad_zamin;
+}player2;
+
+struct  player3{
+	int sarbaz;
+	int tedad_zamin;
+}player3;
+
+struct  player4{
+	int sarbaz;
+	int tedad_zamin;
+}player4;
+
+struct zamin{
+	char esm[7][6][1];
+	char malek[7][6][1] ;
+	int tedad_sarbaz[7][6];
+}zamin;
+
 int main(void) {
+	
+	int tedad_bazikon;
+	
 	
 	al_init();
 	al_init_font_addon();
@@ -28,19 +83,15 @@ int main(void) {
 	
 	
 	
-	
 	ALLEGRO_DISPLAY *display = al_create_display(LENGHT , WIDTH);
 	ALLEGRO_FONT *font = al_load_ttf_font("arial.ttf" , 24 , 0);
-	ALLEGRO_FONT *exitfont = al_load_ttf_font("arial.ttf" , 10 , 0);
-	ALLEGRO_FONT *tfont = al_load_ttf_font("arial.ttf" , 15 , 0);
-	ALLEGRO_FONT *nobatfont = al_load_ttf_font("arial.ttf" , 10 , 0);
-	ALLEGRO_BITMAP *cursor = al_load_bitmap("cursor.tga");
+	ALLEGRO_FONT *font1 = al_load_ttf_font("arial.ttf" , 10 , 0);
+	ALLEGRO_FONT *font2 = al_load_ttf_font("arial.ttf" , 15 , 0);
 	ALLEGRO_MOUSE_STATE curstate;			// gereftan mogheiat mouse
 	ALLEGRO_KEYBOARD_STATE keystate;		// gereftan keyboard 
 	 
 	
 	
-	al_hide_mouse_cursor(display);			// hide mouse 
 	
 	
 	
@@ -57,69 +108,98 @@ int main(void) {
 		printf("Eror font!\n");
 	}
 	
-	if(!exitfont){
+	if(!font1){
 		printf("Eror font!\n");
 	}
-	if(!cursor){
-		printf("Eror cursor!\n");
+	if(!font2){
+		printf("Eror font!\n");
 	}
 	if(!al_install_keyboard()){
 		printf("Eror keyboard!\n");
 	}
-	if(!nobatfont){
-		printf("Eror font!\n");
+	
+	
+	esmgozari();
+	
+	
+	
+	
+	
+	do{
+		al_clear_to_color(al_map_rgb(255,255,255));
+		
+		al_draw_text(font , al_map_rgb(0,0,0) , 170 , 250 , 0 , "tedad bazikon ra moshakhas konid :");
+		
+		al_draw_filled_rectangle(200 , 300 , 300 , 400 ,al_map_rgb(0,255,0));	
+		al_draw_text(font , al_map_rgb(0,0,0) , 240 , 340 , 0 , "2");
+		
+		
+		al_draw_filled_rectangle(310 , 300 , 410 , 400 ,al_map_rgb(0,255,0));	
+		al_draw_text(font , al_map_rgb(0,0,0) , 350 , 340 , 0 , "3");
+		
+		
+		al_draw_filled_rectangle(420 , 300 , 520 , 400 ,al_map_rgb(0,255,0));	
+		al_draw_text(font , al_map_rgb(0,0,0) , 460 , 340 , 0 , "4");
+		
+		al_get_keyboard_state(&keystate);
+		
+		
+		al_flip_display();
+	}while(!al_key_down(&keystate,ALLEGRO_KEY_TAB));
+	
+	
+	
+	switch (tedad_bazikon){
+		case 2 :{
+			player1.sarbaz = 30;
+			player2.sarbaz = 30;
+			break;
+		}
+		case 3 :{
+			player1.sarbaz = 25;
+			player2.sarbaz = 25;
+			player3.sarbaz = 25;
+			break;
+		}
+		case 4 :{
+			player1.sarbaz = 20;
+			player2.sarbaz = 20;
+			player3.sarbaz = 20;
+			player4.sarbaz = 20;
+			break;
+		}
 	}
-	if(!font){
-		printf("Eror font!\n");
-	}
-	
-	
-	
-	
-	char t[2] = "4";
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	pakhsh_zamin(tedad_bazikon);
+	pakhsh_khodkar_sarbaz();
 	
 	// rgb = red + green + blue = (0-255) + (0 - 255) + (0 -255) 
 	
 
 	do {
 		al_clear_to_color(al_map_rgb(255 , 255 ,255));
+		al_get_mouse_state(&curstate);
+		al_get_keyboard_state(&keystate);
 		
 		draw_map();
+		print();
+
 		al_draw_rectangle(350 , 50 , 470 , 100 , al_map_rgb(0,0,0),2);
-		al_draw_text(tfont ,al_map_rgb(0,0,0) , 355 , 70 , 0 , "nobat bazikon");
-		al_draw_text(tfont ,al_map_rgb(0,0,0) , 450 , 70 , 0 , t);
-		
-		al_draw_rectangle(480 , 62 , 530 , 87 , al_map_rgb(0,0,0),2);
-		al_draw_rectangle(533 , 62 , 583 , 87 , al_map_rgb(0,0,0),2);
-		al_draw_rectangle(586 , 62 , 636 , 87 , al_map_rgb(0,0,0),2);
-		al_draw_text(nobatfont ,al_map_rgb(0,0,0) , 483 , 70 , 0 , "Datryaft");
-		al_draw_text(nobatfont ,al_map_rgb(0,0,0) , 536 , 70 , 0 , "nabard");
-		al_draw_text(nobatfont ,al_map_rgb(0,0,0) , 589 , 70 , 0 , "jabejayi");
-		
-		al_get_mouse_state(&curstate);
-		al_draw_bitmap(cursor , curstate.x , curstate.y , 0);
+		al_draw_text(font2 ,al_map_rgb(0,0,0) , 355 , 70 , 0 , "nobat bazikon");
+		//al_draw_text(font2 ,al_map_rgb(0,0,0) , 450 , 70 , 0 , /* character nobat*/);
 		
 		
-		al_draw_text(exitfont ,al_map_rgb(0,0,0) , 724 , 640 , 0 , "Press Esc to exit");
+		
+		
+		
+		
+		al_draw_text(font1 ,al_map_rgb(0,0,0) , 724 , 640 , 0 , "Press Esc to exit");
 		
 		
 
 		
-		al_get_keyboard_state(&keystate);
 		
-		al_rest(0.01);
 		al_flip_display();
+		al_rest(0.5);
 	}while(!al_key_down(&keystate,ALLEGRO_KEY_ESCAPE));
 	
 	
@@ -129,71 +209,382 @@ int main(void) {
 
 
 void draw_map(){
+	  
 	// drawing darya 
-	al_draw_filled_rectangle(200 , 250 , 300 , 300 , al_map_rgb(0,255,255));	
-	al_draw_filled_rectangle(200 , 300 , 300 , 350 , al_map_rgb(0,255,255));	
-	al_draw_filled_rectangle(300 , 300 , 400 , 350 , al_map_rgb(0,255,255));	
-	al_draw_filled_rectangle(200 , 350 , 300 , 400 , al_map_rgb(0,255,255));	
-	al_draw_filled_rectangle(100 , 450 , 200 , 500 , al_map_rgb(0,255,255));	
-	al_draw_filled_rectangle(200 , 450 , 300 , 500 , al_map_rgb(0,255,255));
-	al_draw_filled_rectangle(500 , 350 , 600 , 400,  al_map_rgb(0,255,255));	
-	al_draw_filled_rectangle(600 , 350 , 700 , 400 , al_map_rgb(0,255,255));	
-	al_draw_filled_rectangle(500 , 400 , 600 , 450,  al_map_rgb(0,255,255));	
-	al_draw_filled_rectangle(600 , 400 , 700 , 450 , al_map_rgb(0,255,255));
-	al_draw_filled_rectangle(500 , 450 , 600 , 500,  al_map_rgb(0,255,255));	
-	al_draw_filled_rectangle(600 , 450 , 700 , 500 , al_map_rgb(0,255,255));
-	al_draw_filled_rectangle(400 , 450 , 500 , 500,  al_map_rgb(0,255,255));
+	
+	ALLEGRO_COLOR darya = al_map_rgb(0,255,255);
+	
+	al_draw_filled_rectangle(200 , 250 , 300 , 300 , darya );	
+	al_draw_filled_rectangle(200 , 300 , 300 , 350 , darya );	
+	al_draw_filled_rectangle(300 , 300 , 400 , 350 , darya );	
+	al_draw_filled_rectangle(200 , 350 , 300 , 400 , darya );	
+	al_draw_filled_rectangle(100 , 450 , 200 , 500 , darya );	
+	al_draw_filled_rectangle(200 , 450 , 300 , 500 , darya );
+	al_draw_filled_rectangle(500 , 350 , 600 , 400 , darya );	
+	al_draw_filled_rectangle(600 , 350 , 700 , 400 , darya );	
+	al_draw_filled_rectangle(500 , 400 , 600 , 450 , darya );	
+	al_draw_filled_rectangle(600 , 400 , 700 , 450 , darya );
+	al_draw_filled_rectangle(400 , 450 , 500 , 500 , darya );
+	al_draw_filled_rectangle(500 , 450 , 600 , 500 , darya );	
+	al_draw_filled_rectangle(600 , 450 , 700 , 500 , darya );
 	
 	
+
 	// drawing USA
-	al_draw_filled_rectangle(100 , 150 , 200 , 200 , al_map_rgb(128,128,128));  // sarzamin 1
-	al_draw_filled_rectangle(200 , 150 , 300 , 200 , al_map_rgb(128,128,128));	// sarzamin 2
-	al_draw_filled_rectangle(100 , 200 , 200 , 250 , al_map_rgb(128,128,128));	// sarzamin 7
-	al_draw_filled_rectangle(200 , 200 , 300 , 250 , al_map_rgb(128,128,128));	// sarzamin 8
-	al_draw_filled_rectangle(100 , 250 , 200 , 300 , al_map_rgb(128,128,128));	// sarzamin 13
-	al_draw_filled_rectangle(100 , 300 , 200 , 350 , al_map_rgb(128,128,128));	// sarzamin 18
-	al_draw_filled_rectangle(100 , 350 , 200 , 400 , al_map_rgb(128,128,128));	// sarzamin 22
-	al_draw_filled_rectangle(100 , 400 , 200 , 450 , al_map_rgb(128,128,128));	// sarzamin 25
-	al_draw_filled_rectangle(200 , 400 , 300 , 450 , al_map_rgb(128,128,128));	// sarzamin 26
+	
+	ALLEGRO_COLOR amrica =  al_map_rgb(128,128,128);
+	
+	al_draw_filled_rectangle(100 , 150 , 200 , 200 , amrica );  // sarzamin 0.0
+	al_draw_filled_rectangle(200 , 150 , 300 , 200 , amrica );	// sarzamin 0.1
+	al_draw_filled_rectangle(100 , 200 , 200 , 250 , amrica );	// sarzamin 1.0
+	al_draw_filled_rectangle(200 , 200 , 300 , 250 , amrica );	// sarzamin 1.1
+	al_draw_filled_rectangle(100 , 250 , 200 , 300 , amrica );	// sarzamin 2.0
+	al_draw_filled_rectangle(100 , 300 , 200 , 350 , amrica );	// sarzamin 3.0
+	al_draw_filled_rectangle(100 , 350 , 200 , 400 , amrica );	// sarzamin 4.0
+	al_draw_filled_rectangle(100 , 400 , 200 , 450 , amrica );	// sarzamin 5.0
+	al_draw_filled_rectangle(200 , 400 , 300 , 450 , amrica );	// sarzamin 5.1
 	
 	
 	// drawing Europe
 	
-	al_draw_filled_rectangle(300 , 150 , 400 , 200 , al_map_rgb(200,0,0));	// sarzamin 3
-	al_draw_filled_rectangle(400 , 150 , 500 , 200 , al_map_rgb(200,0,0));	// sarzamin 4
-	al_draw_filled_rectangle(300 , 200 , 400 , 250 , al_map_rgb(200,0,0));	// sarzamin 9
-	al_draw_filled_rectangle(400 , 200 , 500 , 250 , al_map_rgb(200,0,0));	// sarzamin 10
-	al_draw_filled_rectangle(300 , 250 , 400 , 300 , al_map_rgb(200,0,0));	// sarzamin 14
-	al_draw_filled_rectangle(400 , 250 , 500 , 300 , al_map_rgb(200,0,0));	// sarzamin 15
+	ALLEGRO_COLOR europe =  al_map_rgb(200,0,0);
+	
+	al_draw_filled_rectangle(300 , 150 , 400 , 200 , europe );	// sarzamin 0.2
+	al_draw_filled_rectangle(400 , 150 , 500 , 200 , europe );	// sarzamin 0.3
+	al_draw_filled_rectangle(300 , 200 , 400 , 250 , europe );	// sarzamin 1.2
+	al_draw_filled_rectangle(400 , 200 , 500 , 250 , europe );	// sarzamin 1.3
+	al_draw_filled_rectangle(300 , 250 , 400 , 300 , europe );	// sarzamin 2.2
+	al_draw_filled_rectangle(400 , 250 , 500 , 300 , europe );	// sarzamin 2.3
 	
 	// drawing Asia
 	
-	al_draw_filled_rectangle(500 , 150 , 600 , 200 , al_map_rgb(0,128,0));	// sarzamin 5
-	al_draw_filled_rectangle(600 , 150 , 700 , 200 , al_map_rgb(0,128,0));	// sarzamin 5
-	al_draw_filled_rectangle(500 , 200 , 600 , 250 , al_map_rgb(0,128,0));	// sarzamin 11
-	al_draw_filled_rectangle(600 , 200 , 700 , 250 , al_map_rgb(0,128,0));	// sarzamin 12
-	al_draw_filled_rectangle(500 , 250 , 600 , 300 , al_map_rgb(0,128,0));	// sarzamin 16
-	al_draw_filled_rectangle(600 , 250 , 700 , 300 , al_map_rgb(0,128,0));	// sarzamin 17
-	al_draw_filled_rectangle(500 , 300 , 600 , 350 , al_map_rgb(0,128,0));	// sarzamin 20
-	al_draw_filled_rectangle(600 , 300 , 700 , 350 , al_map_rgb(0,128,0));	// sarzamin 21
+	ALLEGRO_COLOR asia = al_map_rgb(0,128,0);
+	
+	al_draw_filled_rectangle(500 , 150 , 600 , 200 , asia );	// sarzamin 0.4
+	al_draw_filled_rectangle(600 , 150 , 700 , 200 , asia );	// sarzamin 0.5
+	al_draw_filled_rectangle(500 , 200 , 600 , 250 , asia );	// sarzamin 1.4
+	al_draw_filled_rectangle(600 , 200 , 700 , 250 , asia );	// sarzamin 1.5
+	al_draw_filled_rectangle(500 , 250 , 600 , 300 , asia );	// sarzamin 2.4
+	al_draw_filled_rectangle(600 , 250 , 700 , 300 , asia );	// sarzamin 2.5
+	al_draw_filled_rectangle(500 , 300 , 600 , 350 , asia );	// sarzamin 3.4
+	al_draw_filled_rectangle(600 , 300 , 700 , 350 , asia );	// sarzamin 3.5
 	
 	
 	
 	
 	// drawing africa 
 	
-	al_draw_filled_rectangle(400 , 300 , 500 , 350 , al_map_rgb(255,255,0));	// sarzamin 19
-	al_draw_filled_rectangle(300 , 350 , 400 , 400 , al_map_rgb(255,255,0));	// sarzamin 23
-	al_draw_filled_rectangle(400 , 350 , 500 , 400 , al_map_rgb(255,255,0));	// sarzamin 24
-	al_draw_filled_rectangle(300 , 400 , 400 , 450 , al_map_rgb(255,255,0));	// sarzamin 27
-	al_draw_filled_rectangle(400 , 400 , 500 , 450 , al_map_rgb(255,255,0));	// sarzamin 28
-	al_draw_filled_rectangle(300 , 450 , 400 , 500 , al_map_rgb(255,255,0));	// sarzamin 29
+	ALLEGRO_COLOR africa = al_map_rgb(255,255,0); 
 	
-	for(int i = 1 ; i<=6 ; i++){
-		for(double j =1.5 ; j<=4.5 ; j = j+0.5){
-			al_draw_rectangle(i*100 , j*100 , i*100+100 , j*100+50 , al_map_rgb(0,0,0),2);
+	al_draw_filled_rectangle(400 , 300 , 500 , 350 , africa );	// sarzamin 3.3
+	al_draw_filled_rectangle(300 , 350 , 400 , 400 , africa );	// sarzamin 4.2
+	al_draw_filled_rectangle(400 , 350 , 500 , 400 , africa );	// sarzamin 4.3
+	al_draw_filled_rectangle(300 , 400 , 400 , 450 , africa );	// sarzamin 5.2
+	al_draw_filled_rectangle(400 , 400 , 500 , 450 , africa );	// sarzamin 5.3
+	al_draw_filled_rectangle(300 , 450 , 400 , 500 , africa );	// sarzamin 6.2
+	
+	
+
+	
+	
+	for(int i = 1 ; i<=7 ; i++){
+		for(int j =1 ; j<=6 ; j ++){
+			al_draw_rectangle(j*100 , i*50+100 , j*100+100 , i*50+150 , al_map_rgb(0,0,0),2);
+			
 		}
 	}
 	
 	
 }
+
+
+
+void print(){
+	ALLEGRO_FONT *font = al_load_ttf_font("arial.ttf" , 15 , 0);
+	ALLEGRO_FONT *font1 = al_load_ttf_font("arial.ttf" , 10 , 0);
+	for(int i = 1 ; i<=7 ; i++){
+		for(int j =1 ; j<=6 ; j ++){
+			if(zamin.esm[i-1][j-1][0] != 'D'){
+				char tmp[10];
+				al_draw_text(font1 ,al_map_rgb(0,0,0) , j*100+10 , i*50+110 , 0 , "player ");
+				al_draw_text(font1 ,al_map_rgb(0,0,0) , j*100+40 , i*50+110 , 0 , zamin.malek[i-1][j-1]);
+				itoa(zamin.tedad_sarbaz[i-1][j-1], tmp, 10);
+				al_draw_text(font ,al_map_rgb(0,0,0) , j*100+50 , i*50+120 , 0 ,tmp);
+			}
+		}
+	}
+}
+
+void esmgozari(){
+	
+	// Usa		Black 
+	
+	zamin.esm[0][0][0] = 'B';	// 0
+	zamin.esm[0][1][0] = 'B';	// 1
+	zamin.esm[1][0][0] = 'B';	// 6
+	zamin.esm[1][1][0] = 'B';	// 7
+	zamin.esm[2][0][0] = 'B';	// 12
+	zamin.esm[3][0][0] = 'B';	// 18
+	zamin.esm[4][0][0] = 'B';	// 24
+	zamin.esm[5][0][0] = 'B';	// 30
+	zamin.esm[5][1][0] = 'B';	// 31
+	
+	// Europe	Red  
+	
+	zamin.esm[0][2][0] = 'R';	// 2
+	zamin.esm[0][3][0] = 'R';	// 3
+	zamin.esm[1][2][0] = 'R';	// 8
+	zamin.esm[1][3][0] = 'R';	// 9
+	zamin.esm[2][2][0] = 'R';	// 14
+	zamin.esm[2][3][0] = 'R';	// 15
+	
+	// Asia		Green	
+	
+	zamin.esm[0][4][0] = 'G';	// 4
+	zamin.esm[0][5][0] = 'G';	// 5
+	zamin.esm[1][4][0] = 'G';	// 10
+	zamin.esm[1][5][0] = 'G';	// 11
+	zamin.esm[2][4][0] = 'G';	// 16
+	zamin.esm[2][5][0] = 'G';	// 17
+	zamin.esm[3][4][0] = 'G';	// 22
+	zamin.esm[3][5][0] = 'G';	// 23
+	
+	// Africa	Yellow
+	
+	zamin.esm[3][3][0] = 'Y';	// 21
+	zamin.esm[4][2][0] = 'Y';	// 26
+	zamin.esm[4][3][0] = 'Y';	// 27
+	zamin.esm[5][2][0] = 'Y';	// 32
+	zamin.esm[5][3][0] = 'Y';	// 33
+	zamin.esm[6][2][0] = 'Y';	// 38
+	
+	// Darya 	D
+	
+	zamin.esm[2][1][0] = 'D';	// 13
+	zamin.esm[3][1][0] = 'D';	// 19
+	zamin.esm[3][2][0] = 'D';	// 20
+	zamin.esm[4][1][0] = 'D';	// 25
+	zamin.esm[4][4][0] = 'D';	// 28
+	zamin.esm[4][5][0] = 'D';	// 29
+	zamin.esm[5][4][0] = 'D';	// 34
+	zamin.esm[5][5][0] = 'D';	// 35
+	zamin.esm[6][0][0] = 'D';	// 36
+	zamin.esm[6][1][0] = 'D';	// 37
+	zamin.esm[6][3][0] = 'D';	// 39
+	zamin.esm[6][4][0] = 'D';	// 40
+	zamin.esm[6][5][0] = 'D';	// 41
+	
+	
+	
+}
+
+
+
+void pakhsh_zamin(int tedad_bazikon){
+	
+	
+}
+
+
+
+int daryaft_sarbaz(int tedad_zamin , char bazikon ){
+	int i = tedad_zamin/3;
+	// amrica
+	if(zamin.malek [0][0][0] == bazikon && zamin.malek[0][1][0] == bazikon && zamin.malek[1][0][0] == bazikon && zamin.malek[1][1][0] == bazikon && zamin.malek[2][0][0] == bazikon && zamin.malek[3][0][0] == bazikon && zamin.malek[4][0][0] == bazikon && zamin.malek[5][0][0] == bazikon&& zamin.malek[5][1][0] == bazikon ){
+		i = i+3;
+	}
+	// europe
+	if( zamin.malek[0][2][0] == bazikon && zamin.malek[0][3][0] == bazikon && zamin.malek[1][2][0] == bazikon && zamin.malek[1][3][0] == bazikon && zamin.malek[2][2][0] == bazikon && zamin.malek[2][3][0] == bazikon ){
+		i = i + 4 ;
+	}
+	// asia
+	if( zamin.malek[0][4][0] == bazikon &&zamin.malek[0][5][0] == bazikon && zamin.malek[1][4][0] == bazikon && zamin.malek[1][5][0] == bazikon && zamin.malek[2][4][0] == bazikon && zamin.malek[2][5][0] == bazikon && zamin.malek[3][4][0] == bazikon && zamin.malek[3][5][0] == bazikon ){
+		i = i + 4;
+	}
+	// africa
+	if( zamin.malek[3][3][0] == bazikon && zamin.malek[4][2][0] == bazikon && zamin.malek[4][3][0] == bazikon && zamin.malek[5][2][0] == bazikon && zamin.malek[5][3][0] == bazikon && zamin.malek[6][2][0] == bazikon ){
+		i = i + 4;
+	}
+	return i;
+}
+
+
+
+
+
+
+void pakhsh_khodkar_sarbaz(){
+
+
+	for(int i = 0 ; i<7 ; i++){
+		for(int j=0  ; j<6 ; j++ ){
+			switch(zamin.malek[i][j][0]){
+				case  '1' :{
+					zamin.tedad_sarbaz[i][j] = 1;
+					player1.sarbaz--;
+					break;
+				}
+				case  '2' : {
+					zamin.tedad_sarbaz[i][j] = 1;
+					player2.sarbaz--;
+					break;
+				}
+				case  '3' : {
+					zamin.tedad_sarbaz[i][j] = 1;
+					player3.sarbaz--;
+					break;
+				}
+				case  '4' : {
+					zamin.tedad_sarbaz[i][j] = 1;
+					player4.sarbaz--;
+					break;
+				}
+			}	
+		}
+	}
+}
+
+
+
+
+
+// barresi shavad
+
+/*
+void nabard(char player){
+	
+	while (1){			//? zadan kelid exit 
+		
+		// ? gereftan i , j;
+		int i , j ;
+		int x,y;
+		int tas_hamle[3];
+		int tas_defa[2];
+		
+
+		if(check( i ,j , player , zamin.malek[7][6][0]) == 1){
+			
+					//? gereftan x , y;
+					
+					if(tedad_sarbaz[i][j]>1 && zamin.esm[x][y][0] != 'D' && zamin.malek[x][y] != player){
+						tas_hamle[0] = tas();
+						tas_hamle[1] = tas();
+						tas_hamle[2] = tas();
+						sort(tas_hamle , 3);
+
+						tas_defa[0] = tas();
+						tas_defa[1] = tas();
+						sort(tas_defa , 2);
+						
+						// bad az andaktan tas
+						if(tas_defa[0]>= tas_hamle[0]){
+							zamin.tedad_sarbaz[i][j]--;
+						}
+						else{
+							zamin.tedad_sarbaz[x][y]--;
+						}
+						
+						if(tedad_sarbaz[i][j]>=3 && tedad_sarbaz[x][y]>1){
+							if(tas_defa[1]>= tas_hamle[1]){
+								zamin.tedad_sarbaz[i][j]--;
+							}
+							else{
+								zamin.tedad_sarbaz[x][y]--;
+							}
+						}
+												
+						if(zamin.tedad_sarbaz[x][y] == 0 ){
+							zamin.malek[x][y] = player;
+						}
+					}
+
+				}
+			}
+		}
+	}
+}
+*/
+// barresi shavad
+
+/*
+
+void jagozari_sarbaza1( int tedad_bazikon , int tedad_sarbaz[7][6] ){
+	int i , j;
+	
+	while(player1.sarbaz != 0 && player2.sarbaz != 0 && player3.sarbaz !=0 && player4.sarbaz != 0){
+		for(int i = 0 ; i<tedad_bazikon ; i++){
+			if(player1.sarbaz != 0){
+				// ? get the i , j;
+				if(check == 1){
+					tedad_sarbaz[i][j] ++;
+					player1.sarbaz--;
+				}
+			}
+			if(player2.sarbaz != 0){
+				// ? get the i , j;
+				if(check == 1 ){
+					tedad_sarbaz[i][j]++;
+					player2.sarbaz--;
+				}
+			}
+			if(player3.sarbaz != 0){
+				// ? get the i , j;
+				if(check == 1){
+					tedad_sarbaz[i][j]++;
+					player3.sarbaz--;
+				}
+			}
+			if(player4.sarbaz != 0){
+				// ? get the i , j;
+				if(check == 1 ){
+					tedad_sarbaz[i][j]++;
+					player4.sarbaz--;
+				}
+			}
+		}
+	}
+}
+*/
+
+
+
+
+
+int tas(){
+    int a;
+    srand(time(0));
+    a = rand()%6 + 1;
+    return a;
+}
+
+
+
+
+
+void sort(int array[] , int size) {
+    for(int i = 0 ; i < size ; i++){
+        for(int j = 0 ; j< size-i-1 ; j++){
+        	if(array[j] < array[j+1] ){
+        		int temp = array[j+1];
+        		array[j+1] = array[j];
+				array[j] = temp; 
+			}
+		}
+	}
+}
+
+
+
+
+int check ( int x , int y , char player ){
+	
+	if (zamin.malek[x][y][0] == player){
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+
+
